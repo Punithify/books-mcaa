a6c03220e1b99d742b80ed7045bf2596c5b6af9a
Punithify <punithsp9@gmail.com>
2023-09-12 15:54:20 +0530

    json file added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const booksJsonFile = require('./books.json');
 const port = process.env.PORT || 3001;
 
-app.get('/books', (req, res) => res.json('bookNo:12'));
+app.get('/books', (req, res) => res.json(booksJsonFile));
 
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
